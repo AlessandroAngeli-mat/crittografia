6fe26bf7eb2200aadc2bf58f96c7eb8106ebb084
AlessandroAngeli-mat <alessandro.angeli@mat.tn.it>
2023-03-17 11:35:58 +0100

    edited

--- angeli.py
+++ angeli.py
@@ -9,35 +9,49 @@ from getpass import getpass
 import json
 import os.path
 
+# function that opens the file and decrypts it
+# - path: string containing the path in which the file is located
+# - password: string containing the password to use to derive the key
+# returns the credentials contained in the file
+
 def load_data(path, password):
     with open(path, 'rb') as in_file:
-        # scomponi i dati letti in 4 pezzi, 3 hanno lunghezze precise
+        # from the file, read the salt, the nonce, the tag and the ciphertext
+        # in the order they were saved in
         salt = in_file.read(16)
         nonce = in_file.read(15)
         tag = in_file.read(16)
         ciphertext = in_file.read(-1)
         
-    # rendi i dati leggibili e salva il risultato in 'data'
-    key = scrypt(password, salt, 16, N=2**20, r=8, p=1) # ricava il segreto necessario per "sbloccare" i dati
-    cipher = AES.new(key, AES.MODE_OCB, nonce) # setup della funzione che "sblocca" i dati
-    data = cipher.decrypt_and_verify(ciphertext, tag) # sblocca i dati
+    # derive the key using the given password and the salt found in the file
+    key = scrypt(password, salt, 16, N=2**20, r=8, p=1)
+    # generate cipher
+    cipher = AES.new(key, AES.MODE_OCB, nonce)
+    # decrypt ciphertext
+    data = cipher.decrypt_and_verify(ciphertext, tag)
     try: 
         credentials = json.loads(data.decode('utf-8'))
     except ValueError as err:
         raise IOError(f'data not valid: {str(err)}')
     return credentials
 
+# function that encrypts and saves the file
+# - path: string containing the path in which to save the file
+# - password: string containing the password to use to derive the key
+# - credentials: dict containing the credentials to save
+
 def save_and_exit(path, password, credentials):
     data = json.dumps(credentials, ensure_ascii=False).encode('utf-8')
-    # proteggi 'data' utilizzando opportunamente la password
-    # ricava il segreto necessario per proteggere i dati
     salt = get_random_bytes(16)
+    # derive key from password and salt
     key = scrypt(password, salt, 16, N=2**20, r=8, p=1)
-    cipher = AES.new(key, AES.MODE_OCB)# setup della funzione che proteggere i dati
-    ciphertext, tag = cipher.encrypt_and_digest(data) # proteggi i dati
+    # generate cipher
+    cipher = AES.new(key, AES.MODE_OCB)
+    # encrypt data
+    ciphertext, tag = cipher.encrypt_and_digest(data)
     with open(path, 'wb') as out_file:
-        # salva i dati protetti nel file situato in 'path'
-        # (salvare anche i parametri necessari per sbloccarli)
+        # save protected data in the file found in 'path'
+        # the parameters needed to unlock it are also saved
         out_file.write(salt)
         out_file.write(cipher.nonce)
         out_file.write(tag)
@@ -62,9 +76,12 @@ def search_and_add(query, dic):
                     }
     return dic
 
+# function that logs the user in
+# - username: string containing the username to be hashed
+# - password: string containing the password to be passed to load_data
 
 def log_in(username, password):
-    # deriva il percorso del file associato all'utente
+    # hash the username and look for a file with that name
     blake_hash = BLAKE2b.new(data = username.encode('utf-8'), digest_bytes=64)
     path_file = blake_hash.hexdigest()
     if os.path.exists(path_file):
@@ -77,6 +94,7 @@ def log_in(username, password):
             print('Error loading data:')
             print(err)
             return
+    # if no file is found, ask to craete a new one
     else:
         prompt = 'User not found. Add as new?'
         prompt += '\n(y to continue, anything else to cancel)\n'
@@ -87,6 +105,7 @@ def log_in(username, password):
             return
     prompt = 'Credentials to search:'
     prompt += '\n(leave blank and press "enter" to save and exit)\n'
+    # ask the user the credentials to look for, save the file on empty input
     while True:
         query = input(prompt)
         if query != '':
@@ -105,10 +124,11 @@ while True:
     print('Insert username and password to load data,')
     print('leave blank and press "enter" to exit.')
     username = input('Username: ')
+    # if no username is provided, quit
     if username == '':
         print('Goodbye!')
         exit()
     else:
-        # leggi la password in maniera opportuna
+        # read the password
         password = getpass('Password: ')
         log_in(username, password)
